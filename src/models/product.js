ebb7436372e1786dd8345b708a53560d0d5ffd02
Amogh Bv <108013171+VascoDGa@users.noreply.github.com>
2023-08-13 05:29:31 +0000

    day1 commit

---
+++ src/models/product.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Product name is mandatory"],
+            trim: true,
+            maxLength: [120,"Product name cannot exceed 120 characters"]
+        },
+        price: {
+            type: Number,
+            required: [true, "Product price is mandatory"],
+            maxLength: [5,"Product price cannot exceed 5 characters"]
+        },
+        description: {
+            type:String
+        },
+        photos: [
+            {
+                secure_url: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+        stock: {
+            type: Number,
+            default: 0
+        },
+        sold: {
+            type: Number,
+            default: 0
+        },
+        collectionId: {
+            ref: "Collection"
+        }
+}, {timestamps: true}
+)
+
+export default mongoose.model("Product", productSchema)
