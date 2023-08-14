d7507de2fcf315d96d7c6125571d91af9c98be44
Amogh Bv <108013171+VascoDGa@users.noreply.github.com>
2023-08-14 18:02:33 +0000

    login and logout functionalities

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -39,7 +39,49 @@ export const signUp = asyncHandler(async (req, res) => {
         user
     })
 
+})
 
 
 
+export const logIN = asyncHandler(async(req,res) => {
+
+    const {email, password} = req.body
+
+    if(!email|| !password){
+        throw new customErrors("Enter valid credentials", 400)
+    }
+
+    const user = User.findOne({email}).select("+password")
+
+    if(!user){
+        throw new customErrors("Invalid password", 400)
+    }
+
+    const isPasswordMatched = await user.comparePassword(password)
+
+    if(isPasswordMatched){
+        const token = user.getJWTtoken()
+        user.password = undefined
+        res.cookie("token", token ,cookieOption)
+        res.status(200).json({
+            success: true,
+            token,
+            user
+        })
+    }
+
+    throw new customErrors("Password is incorrect", 400)
+
+})
+
+export const logOut = asyncHandler(async(req, res) => {
+    res.cookie("token",null,{
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Logged out successfully"
+    })
 })
